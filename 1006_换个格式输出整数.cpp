baabb9d0534c1ecddd7149db09855341eb56c94a
绿皮的巴巴爸爸 <72194922+acharui@users.noreply.github.com>
2021-05-16 22:06:47 +0800

    Add files via upload

---
+++ 1006_换个格式输出整数.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+    uint16_t bai = num / 100;
+    uint16_t shi = num / 10 % 10;
+    uint16_t ge = num % 10;
+
+    while(bai--)
+        cout << 'B';
+    while(shi--)
+        cout << 'S';
+    for(int i = 1; ge > 0; ge--){
+        cout << i;
+        i++;
+    } 
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
